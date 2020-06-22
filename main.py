b19cb9f04f15c0ad95dc336ab7418a506542e773
yuu yamashita <yuu@MacBook-Pro.local>
2020-06-22 17:20:55 +0900

    make it better

--- main.py
+++ main.py
@@ -61,10 +61,14 @@ def callback():
 # LINEでMessageEvent(普通のメッセージが送信されたとき)が起こったときdef以下を実行
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
     msg = event.message.text.encode('utf-8')
+    broadcast_line_msg(msg)
+
+    msg = event.message.text.encode('utf-8')
+    broadcast_line_msg(msg)
 
     if msg in msg_list:
-        print(msg)
         broadcast_line_msg(msg)
         publish_control_msg(msg)
     else:
